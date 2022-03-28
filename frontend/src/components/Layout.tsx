17803d5428c920dd332d4e390c85f0905a62f1b7
yogjin <youngjin988@gmail.com>
2022-03-28 20:26:17 +0900

    feat: Layout(헤더)에 Main과 MyAnimal로 route하는 기능 추가

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const Layout: FC = ({ children }) => {
   return (
     <>
-      <div>헤더</div>
+      <div>
+        <Link to="/">Main</Link>
+        <Link to="/my-animal">My Animal</Link>
+      </div>
       <div>{children}</div>
     </>
   );
