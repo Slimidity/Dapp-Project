116ea8497a1c7e4bfe31046dab1f93ee0816f8c6
yogjin <youngjin988@gmail.com>
2022-03-28 21:32:04 +0900

    feat(MyAnimal.tsx): 해당 account가 가진 nft들을 보여주는 기능 추가

--- frontend/src/components/MyAnimal.tsx
+++ frontend/src/components/MyAnimal.tsx
@@ -1,11 +1,57 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { mintAnimalTokenContract } from '../contracts';
+import AnimalCard from './AnimalCard';
 
 interface MyAnimalProps {
   account: string;
 }
 
 const MyAnimal: FC<MyAnimalProps> = ({ account }) => {
-  return <div>My Animal list</div>;
+  const [animalCards, setAnimalCards] = useState<string[]>([]);
+
+  const getAnimalTokens = async () => {
+    try {
+      if (!account) return;
+
+      // account가 가진 nft 수
+      const balanceLength: string = await mintAnimalTokenContract.methods //
+        .balanceOf(account)
+        .call();
+
+      // 가진 nft 수만큼 반복돌려서 nft들의 type얻기
+      const tempAnimalCards: string[] = [];
+      for (let i = 0; i < parseInt(balanceLength); i++) {
+        const animalTokenId: string = await mintAnimalTokenContract.methods //
+          .tokenOfOwnerByIndex(account, i)
+          .call();
+        const animalTokenType: string = await mintAnimalTokenContract.methods //
+          .animalTypes(animalTokenId)
+          .call();
+
+        tempAnimalCards.push(animalTokenType);
+      }
+
+      // setstate
+      setAnimalCards(tempAnimalCards);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAnimalTokens();
+  }, []);
+
+  return (
+    <>
+      <div>My Animal list</div>
+      <div>
+        {animalCards.map((animalType, index) => (
+          <AnimalCard animalType={animalType} key={index} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default MyAnimal;
