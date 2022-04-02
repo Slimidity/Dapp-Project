cec16d7b46e7ea582b05d06e9c3675e671183de0
yogjin <youngjin988@gmail.com>
2022-04-03 03:40:49 +0900

    feat: nft 판매 등록 후 등록한 가격을 바로 보여주는 기능 추가

--- frontend/src/components/MyAnimalCard.tsx
+++ frontend/src/components/MyAnimalCard.tsx
@@ -23,6 +23,7 @@ const MyAnimalCard: FC<MyAnimalCardProps> = ({
   account,
 }) => {
   const [sellPrice, setSellPrice] = useState<string>('');
+  const [myAnimalPrice, setMyAnimalPrice] = useState<string>(animalPrice); // 판매 동록 후 가격을 바로 보여주기 위한 state
 
   const onPriceChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSellPrice(e.target.value);
@@ -32,19 +33,23 @@ const MyAnimalCard: FC<MyAnimalCardProps> = ({
     if (!account || !saleStatus) return;
 
     const response = await saleAnimalTokenContract.methods //
-      .setForSaleAnimalToken(animalTokenId, web3.utils.toWei(sellPrice))
+      .setForSaleAnimalToken(
+        animalTokenId,
+        web3.utils.toWei(sellPrice, 'ether')
+      )
       .send({ from: account });
 
     if (response.status) {
-      console.log(response);
+      // toWei로 set해야 fromWei할때 형변환 에러나지 않는다.
+      setMyAnimalPrice(web3.utils.toWei(sellPrice, 'ether'));
     }
   };
   // 가격이 0이 아니면 판매중이라는 뜻
   return (
     <div className={styles.container}>
       <AnimalCard animalType={animalType} />
-      {animalPrice !== '0' ? (
-        <div>{`${web3.utils.fromWei(animalPrice)} MATIC`}</div>
+      {myAnimalPrice !== '0' ? (
+        <div>{`${web3.utils.fromWei(myAnimalPrice)} MATIC`}</div>
       ) : (
         <div className={styles.sellContainer}>
           <div className={styles.inputContainer}>
