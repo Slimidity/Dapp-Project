6b1885626ed66f0a5585b06bb8436caf488df655
yogjin <youngjin988@gmail.com>
2022-04-03 02:55:53 +0900

    style: token 판매하는 부분 layout 추가

--- frontend/src/components/MyAnimalCard.tsx
+++ frontend/src/components/MyAnimalCard.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { web3 } from '../contracts';
 import AnimalCard from './AnimalCard';
+import styles from './MyAnimalCard.module.css';
 
 // AnimalCard interface
 export interface IMyAnimalCard {
@@ -18,12 +19,20 @@ const MyAnimalCard: FC<MyAnimalCardProps> = ({
 }) => {
   // 가격이 0이 아니면 판매중이라는 뜻
   return (
-    <div>
+    <div className={styles.container}>
       <AnimalCard animalType={animalType} />
       {animalPrice !== '0' ? (
         <div>{`${web3.utils.fromWei(animalPrice)} MATIC`}</div>
       ) : (
-        <div>판매하기 버튼</div>
+        <div className={styles.sellContainer}>
+          <div className={styles.inputContainer}>
+            <input type="number" />
+            <div>
+              <span>Matic</span>
+            </div>
+          </div>
+          <button className={styles.sellButton}>Sell</button>
+        </div>
       )}
     </div>
   );
